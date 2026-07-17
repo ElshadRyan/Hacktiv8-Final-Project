1a6ab0e8e381207c90de08cc646e51b5250c6884
ElshadRyan <117740569+ElshadRyan@users.noreply.github.com>
2026-07-18 02:47:39 +0700

    push

--- final-project/db/models/vendorModel.ts
+++ final-project/db/models/vendorModel.ts
@@ -1,6 +1,6 @@
 import * as z from "zod";
 import { hashSync } from "bcryptjs";
-import { database } from "@/db/config/mongoDb";
+import { database } from "@/db/config/mongodb";
 
 const VendorSchema = z.object({
   namaToko: z.string().min(1, { message: "Nama Toko is required" }),
@@ -41,7 +41,7 @@ export default class VendorModel {
     const agg = [
       {
         $match: {
-          _id: new ObjectId(vendorId),
+          _id: new ObjectI(vendorId),
         },
       },
       {
